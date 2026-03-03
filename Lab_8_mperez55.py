eed0c45c81cb8fffdabdbc2dd763e39ae56870f5
Marvin P <MarvinPerez1503@outlook.com>
2026-03-03 15:29:00 -0500

    print options created

--- Lab_8_mperez55.py
+++ Lab_8_mperez55.py
@@ -3,3 +3,4 @@ from circle import circumference
 from rectangle import area as rarea
 from rectangle import perimeter
 
+print("Select and option\n1.-Area of a circle\n2.-Circumference of a circle\n3.-Area of a rectangle\n4.-Perimeter of a rectangle\n5.-Exit")
